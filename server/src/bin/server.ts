c58ccbd4fb84380dce51a2e0b853d587cc478ff8
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-11-01 15:30:07 -0300

    feat: set up CORS

--- server/src/bin/server.ts
+++ server/src/bin/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -11,6 +12,10 @@ async function bootstrap(){
      */
     const fastify = Fastify({
         logger: true,
+    })
+
+    await fastify.register(cors, {
+        origin: true,
     })
 
     fastify.get('/pools/count', async (request, reply) => {
@@ -27,7 +32,7 @@ async function bootstrap(){
     /**
      * Starting th server
      */
-    await fastify.listen({ port: 3333 })
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
