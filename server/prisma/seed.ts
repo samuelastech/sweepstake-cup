ec8a282c8709b40ac4f759e4e0ea0dc53f81a133
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-12-28 16:47:47 -0300

    test: database seed
    Some data to help test and maintain the application

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'john.doe@gmail.com',
+            avatarUrl: 'https://github.com/samuelastech.png'
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+            participants: {
+                create: {
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-12-29T12:00:00.201Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'DE'
+        }
+    })
+
+    await prisma.game.create({
+        data: {
+            date: '2022-12-30T12:00:00.201Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses: {
+                create: {
+                    firstTeamPoints: 5,
+                    secondTeamPoints: 0,
+
+                    participant: {
+                        connect: {
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+main()
