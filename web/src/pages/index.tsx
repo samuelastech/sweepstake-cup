59b7a0ea3e64fd11f8bab1162ef5145b2a1691c6
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-12-28 20:31:17 -0300

    feat: connect to the API
    Dinamically bringing information from database

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,13 +1,20 @@
 interface HomeProps {
-	pools: number;
+	poolCount: number;
+	guessCount: number;
+	userCount: number;
 }
 
 import Image from 'next/image'
 
+/**
+ * Assets
+ */
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 import appPreviewImg from '../assets/app-nlw-copa-preview.png'
 import iconCheckImg from '../assets/icon-check.svg'
 import logoImg from '../assets/logo.svg'
+
+import { api } from '../lib/axios'
 
 export default function Home(props: HomeProps) {
 	return (
@@ -22,7 +29,7 @@ export default function Home(props: HomeProps) {
 				<div className='mt-10 flex items-center gap-2'>
 					<Image src={usersAvatarExampleImg} alt='' quality={100}/>
 					<strong className='text-gray-100 text-xl'>
-						<span className='text-ignite-500'>+12.592</span> pessoas já estão usando
+						<span className='text-ignite-500'>+{props.userCount}</span> pessoas já estão usando
 					</strong>
 				</div>
 
@@ -57,7 +64,7 @@ export default function Home(props: HomeProps) {
 					<div className='flex items-center gap-6'>
 						<Image src={iconCheckImg} alt='Check' />
 						<div className='flex flex-col'>
-							<span className='font-bold text-2xl'>+2.034</span>
+							<span className='font-bold text-2xl'>+{props.poolCount}</span>
 							<span>Bolões criados</span>
 						</div>
 					</div>
@@ -67,7 +74,7 @@ export default function Home(props: HomeProps) {
 					<div className='flex items-center gap-6'>
 						<Image src={iconCheckImg} alt='Check' />
 						<div className='flex flex-col'>
-							<span className='font-bold text-2xl'>+192.847</span>
+							<span className='font-bold text-2xl'>+{props.guessCount}</span>
 							<span>Palpites enviados</span>
 						</div>
 					</div>
@@ -82,14 +89,17 @@ export default function Home(props: HomeProps) {
  * Not getting response from Server
  */
 export const getServerSideProps = async () => {
-	const response = await fetch('http://localhost:3333/pools/count')
-	const data = await response.json()
-
-	console.log(data)
+	const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
+		api.get('pools/count'),
+		api.get('guesses/count'),
+		api.get('users/count')
+	])
 
 	return {
 		props: {
-			pools: data.pools 
+			poolCount: poolCountResponse.data.pools,
+			guessCount: guessCountResponse.data.guesses,
+			userCount: userCountResponse.data.users,
 		}
 	}
 }
