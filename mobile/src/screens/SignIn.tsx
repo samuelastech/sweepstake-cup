8a63b15bbe06ad5b4d220799a844bc0047163f2e
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-12-30 11:58:32 -0300

    feat: import logo

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,9 +1,11 @@
 import { Center, Text } from 'native-base'
 
+import Logo from '../assets/logo.svg'
+
 export default function SignIn(){
     return (
         <Center flex={1} bgColor="gray.900">
-            <Text color="white" fontSize={24} fontFamily="Roboto">Sign In</Text>
+           <Logo width={212} height={40} />
         </Center>
     )
 }
