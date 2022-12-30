360ef5b479cdf4115f8404dc5706c335d292fae8
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-12-30 16:57:49 -0300

    feat: authentication with google

--- mobile/src/context/AuthContext.tsx
+++ mobile/src/context/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from 'expo-auth-session/providers/google'
+import * as AuthSession from 'expo-auth-session'
+import * as WebBrowser from 'expo-web-browser'
+
+WebBrowser.maybeCompleteAuthSession()
 
 interface UserProps {
     name: string
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
     user: UserProps
+    isUserLoading: boolean
     signIn: () => Promise<void>
 }
 
@@ -17,17 +23,42 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthProviderProps){
-    async function signIn(){
+    const [user, setUser] = useState<UserProps>({} as UserProps)
+    const [isUserLoading, setIsUserLoading] = useState(false)
 
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '...',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    })
+
+    async function signIn(){
+        try {
+            setIsUserLoading(true)
+            await promptAsync()
+        } catch (error) {
+            console.log(error)
+            throw error
+        } finally {
+            setIsUserLoading(false)
+        }
     }
+
+    async function signInWithGoogle(accessToken: string){
+        console.log(accessToken)
+    }
+
+    useEffect(() => {
+        if(response?.type === 'success' && response.authentication?.accessToken){
+            signInWithGoogle(response.authentication.accessToken)
+        }
+    }, [response])
 
     return (
         <AuthContext.Provider value={{
             signIn,
-            user: {
-                name: 'Samuel',
-                avatarUrl: 'https://github.com/samuelastech.png'
-            }
+            isUserLoading,
+            user
         }}>
             {children}
         </AuthContext.Provider>
