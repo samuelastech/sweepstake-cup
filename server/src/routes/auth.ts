8d11b58332ecfb4b6507ab4031f0f4dfe6be1582
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-12-31 11:48:02 -0300

    feat: retrive google's user from access token

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -4,7 +4,48 @@ import prisma from '../lib/prisma'
 import * as z from 'zod'
 
 async function authRoutes(fastify: FastifyInstance) {
-    
+    fastify.post('/users', async (request, reply) => {
+        try {
+            /**
+             * Validations
+             */
+            const createUserBody = z.object({
+                access_token: z.string()
+            })
+
+            const userInfoSchema = z.object({
+                id: z.string(),
+                email: z.string().email(),
+                name: z.string(),
+                picture: z.string().url()
+            })
+            
+            /**
+             * Fetching user's access token from Google
+             */
+            const { access_token } = createUserBody.parse(request.body)
+            const userResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${access_token}`
+                }
+            })
+
+            const userData = await userResponse.json()
+            const userInfo = userInfoSchema.parse(userData)
+            return { userInfo }
+        } catch (error) {
+            if (error instanceof z.ZodError){
+                reply
+                    .code(400)
+                    .send({
+                        status: false,
+                        message: error.issues[0].message,
+                        invalidFields: error.issues[0].path[0]
+                    })
+            }
+        }
+    })
 }
 
 export default authRoutes
