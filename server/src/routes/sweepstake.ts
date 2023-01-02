efc27b0682a2b851cd784dcddbf7e9cdf72a6800
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-02 14:36:07 -0300

    feat: join a sweepstake

--- server/src/routes/sweepstake.ts
+++ server/src/routes/sweepstake.ts
@@ -4,6 +4,8 @@ import prisma from '../lib/prisma'
 import * as z from 'zod'
 import ShortUniqueId from 'short-unique-id'
 
+import authenticate from '../plugins/authenticate'
+
 async function sweepstakeRoutes(fastify: FastifyInstance) {
     fastify.get('/sweepstakes/count', async (request, reply) => {
         const sweepstakes = await prisma.sweepstake.count()
@@ -73,6 +75,61 @@ async function sweepstakeRoutes(fastify: FastifyInstance) {
             }
         }
 
+    })
+
+    fastify.post('/pools/:id/join', { onRequest: [authenticate] }, async (request, reply) => {
+        const joinPoolBody = z.object({
+            code: z.string()
+        })
+
+        const { code } = joinPoolBody.parse(request.body)
+
+        try {
+            const sweepstake = await prisma.sweepstake.findUnique({
+                where: { code },
+                include: { 
+                    participants: {
+                        where: {
+                            userId: request.user.sub
+                        }
+                    }
+                }
+            })
+
+            if(!sweepstake)
+                throw new Error('Pool not found')
+
+            if(sweepstake.participants.length > 0)
+                throw new Error('You already joined this sweepstake')
+            
+            // Everything's fine
+            await prisma.participant.create({
+                data: {
+                    sweepstakeId: sweepstake.id,
+                    userId: request.user.sub
+                }
+            })
+
+            return reply.status(201).send({
+                status: true,
+                message: 'User joined the sweepstake'
+            })
+            
+        } catch (error) {
+            if(error instanceof Error && error.message === 'Pool not found'){
+                return reply.code(404).send({
+                    status: false,
+                    message: error.message
+                })
+            }
+
+            if(error instanceof Error && error.message === 'You already joined this sweepstake'){
+                return reply.code(400).send({
+                    status: false,
+                    message: error.message
+                })
+            }
+        }
     })
 }
 
