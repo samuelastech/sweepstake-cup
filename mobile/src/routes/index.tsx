92e4841504d0a0da244fef6950b65115c33aef47
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-02 18:47:38 -0300

    feat: sign in and other app routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native'
+import { useAuth } from '../hooks/useAuth'
 
 import SignIn from '../screens/SignIn'
 
 import AppRoutes from './app.route'
 
 export default function Routes(){
+    const { user } = useAuth()
+
     return (
         <NavigationContainer>
-            <SignIn />
+            { user.name ? <AppRoutes /> : <SignIn /> }
         </NavigationContainer>
     )
 }
