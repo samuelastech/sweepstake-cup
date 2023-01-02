bfcccbd8c04d9cb8e24913a898344efc42747198
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-02 17:40:34 -0300

    chore: override ReactNavigation default RootParamList

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            new: undefined,
+            sweepstakes: undefined,
+            find: undefined,
+            details: {
+                id: string
+            }
+        }
+    }
+}
