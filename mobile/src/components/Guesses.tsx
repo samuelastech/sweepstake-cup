8f6bb8ebaf731178f11ca68119e6514c6f698605
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-02 22:33:36 -0300

    chore: add dayjs

--- mobile/src/components/Guesses.tsx
+++ mobile/src/components/Guesses.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { api } from '../services/api';
 import { createToast } from '../utils';
 import { Game, GameProps } from './Game';
+import Loading from './Loading';
 
 interface Props {
 	sweepstackId: string;
@@ -28,9 +29,31 @@ export function Guesses({ sweepstackId }: Props) {
 		}
 	}
 
+	async function handleGuessConfirm(gameId: string) {
+		try {
+			if(!firstTeamPoints.trim() || !secondTeamPoints.trim()){
+				return createToast(toast, 'Informe os placares')
+			}
+
+			await api.post(`/sweepstakes/${sweepstackId}/games/${gameId}/guesses`, {
+				firstTeamPoints: Number(firstTeamPoints),
+				secondTeamPoints: Number(secondTeamPoints),
+			})
+
+			createToast(toast, 'Palpite realizado com sucesso', false)
+
+			fetchGames()
+		} catch (error) {
+			console.log(error)
+			createToast(toast, 'Não foi possível enviar o palpite')
+		}
+	}
+
 	useEffect(() => {
 		fetchGames()
 	}, [sweepstackId])
+
+	if(isLoading) return <Loading />
 
 	return (
 		<FlatList
@@ -41,7 +64,7 @@ export function Guesses({ sweepstackId }: Props) {
 					data={item}
 					setFirstTeamPoints={setFirstTeamPoints}
 					setSecondTeamPoints={setSecondTeamPoints}
-					onGuessConfirm={() => {}}
+					onGuessConfirm={() => handleGuessConfirm(item.id)}
 				/>
 			)}
 		/>
